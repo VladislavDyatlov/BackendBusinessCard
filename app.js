ee595fdc18bd64cea56368e53ea8cdf795c21745
Владислав Дятлов <datlovvladislav6@gmail.com>
2023-08-07 20:50:31 +0300

    second commit

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 require('dotenv').config();
 const app = express();
+const { prisma } = require('../prisma/prisma-client.js');
 const cors = require('cors');
 
 app.use(cors());
@@ -14,7 +15,12 @@ app.use(cookieParser());
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use('/api/user', async(req, res) => {
-    res.status(200).json({ "project: projects": "jj" });
+    const projects = await prisma.project.findMany({
+        include: {
+            image: true,
+        }
+    });
+    res.status(200).json({ project: projects });
 });
 
 module.exports = app;
