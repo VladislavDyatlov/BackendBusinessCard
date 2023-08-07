98d99c01b08b99a5e6ec2f81e59520dd18ea3759
Владислав Дятлов <datlovvladislav6@gmail.com>
2023-08-07 17:02:19 +0300

    second commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use('/api/user', require('./routes/users'));
 app.use('/api/article', require('./routes/state'));
